f055651cc66b77b20d2fd9707e5e968d48740402
jaychou2211 <cumonkeyte@gmail.com>
2024-10-27 00:15:48 +0800

    refactor : Give more information within getOrder.

--- apps/core-commerce/src/orders/database/order-query-handler.ts
+++ apps/core-commerce/src/orders/database/order-query-handler.ts
@@ -1,8 +1,11 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
 import { IOrderQueryHandler } from "./order-query-handler.interface";
 import { DataSource } from "typeorm";
 import { OrderModel } from "./models/order.model";
 import { OrderEventModel } from "./models/order-event.model";
+import { ShipmentModel } from "./models/shipment.model";
+import { OrderItemModel } from "./models/order-item.model";
+import { groupBy } from "lodash";
 
 @Injectable()
 export class OrderQueryHandler implements IOrderQueryHandler {
@@ -12,7 +15,19 @@ export class OrderQueryHandler implements IOrderQueryHandler {
   ) { }
 
   async findById(id: string): Promise<any> {
-    return this.dataSource.getRepository(OrderModel).findOne({ where: { id } });
+    const order = await this.dataSource.getRepository(OrderModel).findOne({ where: { id } });
+    if (!order) throw new NotFoundException(`Order with ID ${id} does not exist`);
+    const shipments = await this.dataSource.getRepository(ShipmentModel).find({ where: { orderId: id } });
+    const orderItems = await this.dataSource.getRepository(OrderItemModel).find({ where: { orderId: id } });
+    const shipmentOrderItemsMap = groupBy(orderItems, 'shipmentId');
+    return { 
+      order, 
+      shipments: shipments.map(shipment => ({
+        ...shipment,
+        itemIds: shipmentOrderItemsMap[shipment.id].map(item => item.id)
+      })), 
+      orderItems 
+    };
   }
 
   async getOrdersHistory(id: string): Promise<any[]> {
